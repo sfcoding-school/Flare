cf911cba6f4949e8e0c24ad3c54c4bf055df2a7a
andrea29292 <andrea.ceccarelli29292@gmail.com>
2014-07-03 20:49:43 +0200

    user.getID()

--- app/src/main/java/com/sfcoding/flare/MainActivity.java
+++ app/src/main/java/com/sfcoding/flare/MainActivity.java
@@ -1,17 +1,69 @@
 package com.sfcoding.flare;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.facebook.*;
+import com.facebook.model.*;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // start Facebook Login
+        Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+            // callback when session changes state
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                if (session.isOpened()) {
+                    // make request to the /me API
+                    Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+                        // callback after Graph API response with user object
+                        @Override
+                        public void onCompleted(GraphUser user, Response response) {
+                            if (user != null) {
+                                TextView welcome = (TextView) findViewById(R.id.welcome);
+                                welcome.setText("Hello " + user.getId() + "!");
+                            }
+                        }
+                    }).executeAsync();
+                }
+            }
+        });
+        //get hash key
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.partymanager", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String sign = Base64.encodeToString(md.digest(), Base64.DEFAULT);
+                Log.e("MY KEY HASH:", sign);
+                //Toast.makeText(getApplicationContext(), sign, Toast.LENGTH_LONG).show();
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("ProfileActivity:", "PackageManager.NameNotFoundExceptio" + e);
+            //Toast.makeText(getApplicati
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("ProfileActivity:", "NoSuchAlgorithmException" + e);
+        }
     }
 
 
@@ -32,5 +84,11 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
     }
 }
